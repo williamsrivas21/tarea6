73c66dd3c79e0706d62ea35d949aa79f6e9b9a08
WILLIAMS <WILLIAMS@WilliamsRivas>
2021-07-15 13:04:10 -0500

    Primer Comit para repositor remoto =

---
+++ src/verdad_falso/Verdad_falso.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package verdad_falso;
+
+/**
+ *
+ * @author WILLIAMS
+ */
+public class Verdad_falso {
+
+  
+    public static void main(String[] args) {
+     int M = 6;
+     int T = 1;
+     int K = -10;
+       
+     double divicion = (double)T/ (double)K;
+     int suma = M + T;
+     int resta = M-T;
+    
+      
+      
+      System.out.println("\nLa divicion es:"+divicion+"\nEl resultado de la suma es:"+suma+"\nLa resta es:"+ resta);
+    
+   
+    }
+    
+}
